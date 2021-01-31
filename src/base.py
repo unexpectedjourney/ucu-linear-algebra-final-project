1b827aa85bf89d846a796f3345dc2ff6a72f21f5
Anton Babenko <antonwarbeck123@gmail.com>
2021-01-31 14:53:23 +0200

    Added base file for general structure of models

---
+++ src/base.py
@@ -0,0 +1,9 @@
+class BaseModel:
+    def fit(self, X, y=None, *args, **kwargs):
+        raise NotImplementedError("fit method is not implemented yet")
+
+    def transform(self, X):
+        raise NotImplementedError("transform method is not implemented yet")
+
+    def fit_transform(self, X, y=None, *args, **kwargs):
+        raise NotImplementedError("fit_transform method is not implemented yet")
