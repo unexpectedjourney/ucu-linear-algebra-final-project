6d0b4a87fb35a94d84b7f3170ad6d2330bba56f6
Anton Babenko <antonwarbeck123@gmail.com>
2021-02-08 22:45:46 +0200

    Added test data apply to the system + added prediction method to base class

--- src/base.py
+++ src/base.py
@@ -7,3 +7,6 @@ class BaseModel:
 
     def fit_transform(self, X, y=None, *args, **kwargs):
         raise NotImplementedError("fit_transform method is not implemented yet")
+
+    def predict(self, X):
+        raise NotImplementedError("predict method is not implemented yet")
